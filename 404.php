cd71ee48b1a074b8250af722322e807800d56afa
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-05-12 12:24:14 -0400

    Edit margins of 404 page

--- 404.php
+++ 404.php
@@ -3,9 +3,9 @@
 <div class="main-content">
 <div class="internal-hero-image" style="background-color:#001b3d; background-image: linear-gradient(to bottom, rgba(0, 27, 61, 1), rgba(245, 246, 252, 0.52)), url('https://pwd.aa.ufl.edu/wp-content/uploads/2021/03/0I1A5562-scaled.jpg');">
 </div>
-<div class="container text-center">
+<div class="container mt-5 mb-5 text-center">
   <div class="not-found">
-    <img src="https://pwd.aa.ufl.edu/wp-content/uploads/2021/05/Telescope-Icon-404.png" class="img-fluid" />
+    <img src="https://pwd.aa.ufl.edu/wp-content/uploads/2021/05/Telescope-Icon-404.png" class="img-fluid" style="max-width:300px;"/>
     <h1 class="mt-4">PAGE NOT FOUND</h1>
     <h2>This usually never happens.</h2>
     <p>It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a>?</p>
