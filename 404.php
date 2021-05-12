595a4c7cc4c4309b0a4fe443d0602ceaba81a389
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-05-12 12:25:18 -0400

    Add padding to bottom of 404 text

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@
     <img src="https://pwd.aa.ufl.edu/wp-content/uploads/2021/05/Telescope-Icon-404.png" class="img-fluid" style="max-width:300px;"/>
     <h1 class="mt-4">PAGE NOT FOUND</h1>
     <h2>This usually never happens.</h2>
-    <p>It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a>?</p>
+    <p class="pb-5">It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a>?</p>
   </div>
 </div>
 </div>
