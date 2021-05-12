69d658793ae5e9464ac1163e812e8a6f980f453f
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-05-12 12:31:37 -0400

    Add oops to 404

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@
 <div class="container mt-5 mb-5 text-center">
   <div class="not-found">
     <img src="https://pwd.aa.ufl.edu/wp-content/uploads/2021/05/Telescope-Icon-404.png" class="img-fluid" style="max-width:300px;"/>
-    <h1 class="mt-4">404 NOT FOUND</h1>
+    <h1 class="mt-4">OOPS! 404 NOT FOUND</h1>
     <h2>This usually never happens.</h2>
     <p class="pb-5">It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a>?</p>
   </div>
