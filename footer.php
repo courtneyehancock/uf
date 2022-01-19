3dc93d3f789a54eca934dc5ded0adf7afead868d
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2022-01-19 13:43:33 -0500

    Edit footer columns

--- footer.php
+++ footer.php
@@ -1,17 +1,13 @@
 <footer>
   <div class="container pb-5">
     <div class="row">
-      <div class="col-lg-5">
+      <div class="col-lg-9">
         <!--Logo/text widget-->
         <?php dynamic_sidebar('left-footer'); ?>
       </div>
-      <div class="col-lg-4">
+      <div class="col-lg-3">
         <!--Middle Footer-->
         <?php dynamic_sidebar('middle-footer'); ?>
-      </div>
-      <div class="col-lg-3">
-        <!--Middle Right Footer-->
-        <?php dynamic_sidebar('middle-right-footer'); ?>
       </div>
     </div>
   </div>
@@ -25,7 +21,7 @@
         <a href="https://pwd.aa.ufl.edu/" target="_blank"><img alt="PWD Logo" src="https://pwd.aa.ufl.edu/wp-content/uploads/2020/07/pwd-white.png"></a>
       </div>
       <div class="col-lg-2">
-        <a href="https://www.ufl.edu/" target="_blank"><img alt="UF Logo" src="https://cwde.aa.ufl.edu/wp-content/uploads/sites/2/2020/05/UF_Signature_white.png"/></a>
+        <a href="https://www.ufl.edu/" target="_blank"><img alt="UF Logo" src="https://pwd.aa.ufl.edu/wp-content/uploads/2021/08/UF-SVG.svg"/></a>
       </div>
     </div>
   </div>
