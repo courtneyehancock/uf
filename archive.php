aa4d1ca753f5161921ffaff368afb2ff8e6666d8
courtneyehancock <courtneyehanco1k@TSS-OPWD-001A.local>
2023-06-29 06:08:25 -0400

    Mimick index.php styles for archive.php

--- archive.php
+++ archive.php
@@ -22,39 +22,49 @@ get_header();
  </h1>
 </div>
 </div>
-  <div class="container page-top">
-       <?php
-         if(have_posts()){
-           while(have_posts()){
-             the_post();?>
+<div class="container mt-5 page-top">
+  <div class="row">
+    <div class="col-lg-9">
+  <?php
+    if(have_posts()){
+      while(have_posts()){
+        the_post();?>
 
-             <div class="posts">
-               <div class="row align-items-center shadow p-3 mb-4 rounded-lg bg-white">
-                 <div class="col-lg-4">
-               <?php the_post_thumbnail('medium'); ?>
-             </div>
-             <div class="col-lg-8">
-               <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-               <?php the_excerpt(); ?>
-               <p>    <a href="<?php the_permalink(); ?>" class="btn btn-primary card-btn">READ POST</a>
+        <div class="posts">
+          <div class="row p-3 mb-4">
+            <div class="col-lg-3 mt-3">
+          <?php the_post_thumbnail('medium'); ?>
+        </div>
+        <div class="col-lg-9">
+          <h1 class="post-title lines"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+          <p><?php the_excerpt(); ?> | <a href="<?php the_permalink(); ?>" class="underline-from-left arrow-link">Read Post</a></p>
 
-               <?php
-                 $archive_year = get_the_time('Y');
-                 $archive_month = get_the_time('m');
-                 $archive_day = get_the_time('d');
-               ?>
-               <div class="post-info d-flex align-items-center justify-content-end">
-                 <p class="font-italic">Published: <?php echo get_the_date(); ?></p>
-               <p class="category-label font-italic">Category: <?php the_category(); ?></p>
-               </div>
+          <?php
+            $archive_year = get_the_time('Y');
+            $archive_month = get_the_time('m');
+            $archive_day = get_the_time('d');
+          ?>
+          <div class="post-info d-flex align-items-center">
+            <p><i class="fa-solid fa-calendar-days mr-2"></i> <?php echo get_the_date(); ?></p>
+          <p class="category-label"><?php the_category(); ?></p>
+          </div>
 
-             </div>
-             </div>
-           </div>
-       <?php } //ends while loop
-         }//end if statement
-         ?>
+        </div>
+        </div>
+
+      </div>
+
+
+  <?php } //ends while loop
+    }//end if statement
+    ?>
+    </div>
+    <div class="col-lg-3">
+      <!--Recent Posts-->
+      <?php dynamic_sidebar('recent-posts'); ?>
+
+    </div>
   </div>
 </div>
-
+</div>
 <?php get_footer(); ?>
