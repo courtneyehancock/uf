6ddf446ba5299d2eb2ec2668a970db1a43ff8e01
courtneyehancock <courtneyehanco1k@TSS-OPWD-001A.local>
2024-07-25 15:17:20 -0400

    Remove all.css function and files

--- functions.php
+++ functions.php
@@ -16,9 +16,6 @@
   //main CSS
     wp_enqueue_style('main-styles', get_stylesheet_uri());
 
-  //font awesome
-  	wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/css/all.css');
-
   //Javascript files
     wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/main.js');
     wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js');
@@ -104,7 +101,7 @@
     return 20;
   }
   add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
-  
+
   //Custom Menus
   function custom_menus(){
     register_nav_menus(array(
