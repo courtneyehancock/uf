28d74a21b16afd6e436a2cddad033b6c98f3f1e0
Courtney Glancy <42854186+courtneyehancock@users.noreply.github.com>
2024-12-11 14:46:57 -0500

    Update display of list results

--- search.php
+++ search.php
@@ -1,14 +1,14 @@
 <?php /* Template Name: Search Page */
 get_header();
 ?>
-<div class="main-content pt-5">
+<div class="main-content">
   <div class="container pt-5">
         <?php if(have_posts()){?>
                 <h1><?php printf(__('SEARCH RESULTS FOR : %s'), '<span>' . get_search_query() . '</span>');?></h1>
                 <hr>
                 <?php while(have_posts()){
                   the_post(); ?>
-                  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                  <h3>>> <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                   <?php the_excerpt();
                 }
               }else{?>
