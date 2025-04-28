309a68d91199292dac53998363d11b65ed063e75
Courtney Glancy <42854186+courtneyehancock@users.noreply.github.com>
2025-04-28 06:49:35 -0400

    Remove internal hero image

--- 404.php
+++ 404.php
@@ -1,8 +1,6 @@
 <?php /*Template Name: 404 Page*/?>
 <?php get_header(); ?>
 <div class="main-content">
-<div class="internal-hero-image" style="background-color:#001b3d; background-image: linear-gradient(360deg, #00529be3, #00285ddb, #001a3d), url('https://pwd.aa.ufl.edu/wp-content/uploads/2021/03/0I1A5562-scaled.jpg');">
-</div>
 <div class="container mt-5 mb-5 text-center page-top">
   <div class="not-found">
     <img src="https://pwd.aa.ufl.edu/wp-content/uploads/2021/05/Telescope-Icon-404.png" class="img-fluid" style="max-width:300px;"/>
