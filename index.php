f9d16386bdc85df4588b1f9ba90a1542f777c529
Courtney Glancy <42854186+courtneyehancock@users.noreply.github.com>
2025-06-26 15:46:32 -0400

    Update image header php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ( function_exists('yoast_breadcrumb') ) {
             the_post();?>
             <div class="col-xl-4 mb-3">
             <div class="card">
-              <img class="card-img-top" <?php the_post_thumbnail('medium'); ?>
+              <?php the_post_thumbnail('medium', ['class' => 'card-img-top']); ?>
               <div class="post-info">
                 <p><?php echo get_the_author();?><p>
                 <p><?php echo get_the_date(); ?></p>
