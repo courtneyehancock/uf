26b09ebf7f0c3fdbb4aad729e8105fc54d6bef0f
Courtney Glancy <42854186+courtneyehancock@users.noreply.github.com>
2026-03-12 14:49:59 -0400

    Update aria labels for accessibility

--- single.php
+++ single.php
@@ -10,19 +10,33 @@
         <div class="row d-flex align-items-start">
           <div class="col-lg-9 pr-4 pl-4 pb-4 mb-5">
             <div class="post-feature">
-              <?php the_post_thumbnail(); ?>
+              <?php $thumbnail_id = get_post_thumbnail_id(); 
+        $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true); ?>
+        <?php the_post_thumbnail('large', [ 
+                                 'alt' => esc_attr($alt ?: get_the_title())
+                                 ]); ?>
             </div>
             <div class="post-details mt-3 d-flex align-items-center">
-            <p class="written-by"><?php echo get_the_author(); ?></p>
-            <p class="published"><?php echo get_the_date(); ?></p>
-            <p class="category-label"><?php the_category(); ?></p>
+          <p class="written-by">
+<span class="screen-reader-text">Author: </span><?php echo get_the_author(); ?>
+</p>
+
+<p class="published">
+<span class="screen-reader-text">Published on: </span><?php echo get_the_date('m/d/Y'); ?>
+</p>
+            <p class="category-label">
+<span class="screen-reader-text">Categories: </span>
+<?php the_category(', '); ?>
+</p>
           </div>
             <h1 class="post-title mt-3"><?php the_title(); ?></h1>
             <?php the_content(); ?>
           </div>
           <div class="col-lg-3">
             <!--Recent Posts-->
-            <?php dynamic_sidebar('recent-posts'); ?>
+           <aside aria-label="Recent blog posts">
+<?php dynamic_sidebar('recent-posts'); ?>
+</aside>
 
         </div>
         </div>
